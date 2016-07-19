5e788e5f58432ab0c88016d2f9f5f2ef5afbee4f
Steven Haid <stevenhaid@gmail.com>
2016-07-19 07:24:19 -0400

    commens

--- util_mccdaq.c
+++ util_mccdaq.c
@@ -20,9 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// XXX - TODO
-// - the extra packet ?
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
